fc26de0a3b945f04c2d31425a130743fdde44bf0
Max Edwards <medwards2099@gmail.com>
2019-11-04 14:47:33 -0500

    Done

--- BlueBadgeProject.MVC/App_Start/BundleConfig.cs
+++ BlueBadgeProject.MVC/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace BlueBadgeProject.MVC
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap (1).css",
                       "~/Content/site.css"));
         }
     }
